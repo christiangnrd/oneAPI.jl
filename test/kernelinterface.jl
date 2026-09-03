d6910927ebdf88c90c01c22935e2bab4623e3153
Christian Guinard <28689358+christiangnrd@users.noreply.github.com>
2026-09-03 13:11:00 -0300

    KA 0.10

--- test/kernelinterface.jl
+++ test/kernelinterface.jl
@@ -1,6 +1,5 @@
 import KernelInterface
-using oneAPI.oneAPIInterface
 
 include(joinpath(dirname(pathof(KernelInterface)), "..", "test", "testsuite.jl"))
 
-Testsuite.testsuite(oneAPIInterface.oneAPIBackend, "oneAPI", oneAPI, oneArray, oneAPI.oneDeviceArray)
+Testsuite.testsuite(oneAPIBackend, "oneAPI", oneAPI, oneArray, oneAPI.oneDeviceArray)
